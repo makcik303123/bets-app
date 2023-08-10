d3b1cbdc866552f67a0b885456d51eae2ee1ebb3
Makcik303123 <adoberoflit@gmail.com>
2023-08-11 00:01:12 +0300

    finish match scss

--- src/App.tsx
+++ src/App.tsx
@@ -74,27 +74,58 @@ function App() {
         <div className="table-content">
           <div className="match">
             <div className="match-header">
-              <div>333</div>
-              <div className="test">
+              <div className="match-date">
+                <span className="date-time">19:10</span>
+                <span className="date-day">8 aug.</span>
+              </div>
+              <div className="match-game">
                 <img src="../img/valorant.webp" alt="valorant" />
               </div>
             </div>
             <div className="match-body">
               <div className="select">
-                position: relative; align-items: center;
+                <span className="select-event">Winner of the Game</span>
+                <span className="select-tournament">
+                  2023 [RL] RLCS World Championship
+                </span>
               </div>
-              <div className="teamLeft">
-                position: relative; align-items: center;
+              <div className="teamLeft team">
+                <div className="team-name">Karmine Corp</div>
+                <div className="team-logo">
+                  <img
+                    width={24}
+                    height={24}
+                    src="../img/team-logo.png"
+                    alt="logo"
+                  />
+                </div>
+                <div className="team-btn">1.04</div>
               </div>
               <div className="draw">1:2</div>
-              <div className="teamRight">
-                position: relative; align-items: center;
+              <div className="teamRight team">
+                <div className="team-name">Team Secret</div>
+                <div className="team-logo">
+                  <img
+                    width={24}
+                    height={24}
+                    src="../img/team-logo-1.png"
+                    alt="logo"
+                  />
+                </div>
+                <div className="team-btn">8.28</div>
               </div>
             </div>
             <div className="match-more">
               <button className="more-btn">
-                <span>23</span>
-                <img width={15} height={15} src="../img/down.svg" alt="down" />
+                <span className="more-counter">23</span>
+                <div className="more-down">
+                  <img
+                    width={15}
+                    height={15}
+                    src="../img/down.svg"
+                    alt="down"
+                  />
+                </div>
               </button>
             </div>
           </div>
