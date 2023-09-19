d8ade57cf5a2ce5392229e3ddc18e36e56a226af
Makcik303123 <adoberoflit@gmail.com>
2023-09-19 21:21:06 +0300

    add betslip

--- src/pages/Bets.jsx
+++ src/pages/Bets.jsx
@@ -4,65 +4,80 @@ import GameBar from "../components/GameBar";
 import Panel from "../components/Panel";
 
 function Bets() {
-	const [dataGames, setDataGames] = React.useState([]);
-	const [activeVideoGame, setActiveVideoGame] = React.useState(0);
-	const [isLoading, setIsLoading] = React.useState(true);
-	const arrVideoGames = [
-		["ALL", "GAMES"],
-		"cs-go",
-		"dota-2",
-		"league-of-legends",
-		"lol-wild-rift",
-		"r6-siege",
-		"ow",
-		"starcraft-2",
-		"rl",
-		"call",
-		["MORE", "GAMES"],
-	];
+  const [dataGames, setDataGames] = React.useState([]);
+  const [activeVideoGame, setActiveVideoGame] = React.useState(0);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const arrVideoGames = [
+    ["ALL", "GAMES"],
+    "cs-go",
+    "dota-2",
+    "league-of-legends",
+    "lol-wild-rift",
+    "r6-siege",
+    "ow",
+    "starcraft-2",
+    "rl",
+    "call",
+    ["MORE", "GAMES"],
+  ];
 
-	const filter =
-		typeof arrVideoGames[activeVideoGame] === "string"
-			? "?filter[videogame]=" + arrVideoGames[activeVideoGame]
-			: "";
+  const filter =
+    typeof arrVideoGames[activeVideoGame] === "string"
+      ? "?filter[videogame]=" + arrVideoGames[activeVideoGame]
+      : "";
 
-	React.useEffect(() => {
-		async function fetchData() {
-			try {
-				setIsLoading(true);
-				const liveGamesResponce = await instance.get(
-					`/matches/running${filter}`
-				);
-				const upcommingGamesResponce = await instance.get(
-					`/matches/upcoming${filter}`
-				);
-				setDataGames([
-					...liveGamesResponce.data,
-					...upcommingGamesResponce.data,
-				]);
-				setIsLoading(false);
-			} catch (error) {
-				alert("Error");
-			}
-		}
-		fetchData();
-	}, [filter]);
+  React.useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const liveGamesResponce = await instance.get(
+          `/matches/running${filter}`
+        );
+        const upcommingGamesResponce = await instance.get(
+          `/matches/upcoming${filter}`
+        );
+        setDataGames([
+          ...liveGamesResponce.data,
+          ...upcommingGamesResponce.data,
+        ]);
+        setIsLoading(false);
+      } catch (error) {
+        alert("Error");
+      }
+    }
+    fetchData();
+  }, [filter]);
 
-	console.log(dataGames);
+  console.log(dataGames);
 
-	return (
-		<>
-			<div className="swiper">
-				<img src="../img/slide.png" alt="slide" />
-			</div>
-			<GameBar
-				activeVideoGame={activeVideoGame}
-				setActiveVideoGame={setActiveVideoGame}
-				arrVideoGames={arrVideoGames}
-			/>
-			<Panel dataGames={dataGames} isLoading={isLoading} />
-		</>
-	);
+  const buttons = ["betslip", "history", "quest"];
+  const [activeButton, setActiveButton] = React.useState(0);
+
+  return (
+    <div className="grid">
+      <div className="grid-site"></div>
+      <div className="grid-center">
+        <div className="swiper">
+          <img src="../img/slide.png" alt="slide" />
+        </div>
+        <GameBar
+          activeVideoGame={activeVideoGame}
+          setActiveVideoGame={setActiveVideoGame}
+          arrVideoGames={arrVideoGames}
+        />
+        <Panel dataGames={dataGames} isLoading={isLoading} />
+      </div>
+      <div className="grid-site">
+        <div className="helper">
+          <div className="switcher">
+            {buttons.map((button, i) => (
+              <button className="switcher-button active">{button}</button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Bets;
