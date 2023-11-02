57fee554a8f7a233d1a5064c2de1305e103ce259
Makcik303123 <adoberoflit@gmail.com>
2023-11-02 23:34:45 +0300

    idea solution with amount multi | single

--- src/components/BetSlip/index.jsx
+++ src/components/BetSlip/index.jsx
@@ -8,101 +8,88 @@ import { Scrollbars } from "react-custom-scrollbars-2";
 import { useSelector, useDispatch } from "react-redux";
 
 import {
-	clearBetSlipList,
-	changeListType,
+  clearBetSlipList,
+  changeListType,
 } from "../../redux/slices/betSlipListSlice";
 
 function BetSlip() {
-	const { list, listType, amount } = useSelector(
-		(state) => state.betSlipListReducer
-	);
+  const { list, listType } = useSelector((state) => state.betSlipListReducer);
+  const dispatch = useDispatch();
 
-	const multiplayer =
-		Math.round(list.reduce((acc, item) => acc * item.multiplayer, 1) * 100) /
-		100;
+  console.log(list);
 
-	console.log(list);
-	const dispatch = useDispatch();
+  const switcherButtons = ["single", `multi(${list.length})`];
 
-	const switcherButtons = ["single", `multi(${list.length})`];
+  const listHeight = () => {
+    const betHeight = !!listType ? 90 : 195;
+    return list.length > 3 ? 3 * betHeight : list.length * betHeight;
+  };
 
-	const listHeight = () => {
-		const betHeight = !!listType ? 90 : 195;
-		return list.length > 3 ? 3 * betHeight : list.length * betHeight;
-	};
+  if (!list.length) {
+    return <div className="">not found</div>;
+  }
 
-	if (!list.length) {
-		return <div className="">not found</div>;
-	}
+  return (
+    <div className="bet-slip">
+      <div className="bet-slip__head head">
+        <div className="head__wrapper">
+          <div className="head__switcher">
+            {switcherButtons.map((btn, i) => (
+              <button
+                className={"switcher__btn " + (listType === i ? "active" : "")}
+                onClick={() => dispatch(changeListType(i))}
+                key={i}
+              >
+                {btn}
+              </button>
+            ))}
+          </div>
+          <button
+            className="head__close"
+            onClick={() => dispatch(clearBetSlipList())}
+          >
+            <svg className="medal" width="20" height="20">
+              <use href="./img/icons/sprite.svg#delete-bet-slip"></use>
+            </svg>
+          </button>
+        </div>
+        <div className="auto-accept">
+          <div className="auto-accept__btn">
+            <i className="btn__checked active"></i>
+            <span className="btn__description">Accept all odds changes</span>
+          </div>
+          <div className="auto-accept__info">
+            <svg className="medal" width="20" height="20">
+              <use href="./img/icons/sprite.svg#information-bet-slip"></use>
+            </svg>
+          </div>
+        </div>
+      </div>
 
-	return (
-		<div className="bet-slip">
-			<div className="bet-slip__head head">
-				<div className="head__wrapper">
-					<div className="head__switcher">
-						{switcherButtons.map((btn, i) => (
-							<button
-								className={"switcher__btn " + (listType === i ? "active" : "")}
-								onClick={() => dispatch(changeListType(i))}
-							>
-								{btn}
-							</button>
-						))}
-					</div>
-					<button
-						className="head__close"
-						onClick={() => dispatch(clearBetSlipList())}
-					>
-						<svg className="medal" width="20" height="20">
-							<use href="./img/icons/sprite.svg#delete-bet-slip"></use>
-						</svg>
-					</button>
-				</div>
-				<div className="auto-accept">
-					<div className="auto-accept__btn">
-						<i className="btn__checked active"></i>
-						<span className="btn__description">Accept all odds changes</span>
-					</div>
-					<div className="auto-accept__info">
-						<svg className="medal" width="20" height="20">
-							<use href="./img/icons/sprite.svg#information-bet-slip"></use>
-						</svg>
-					</div>
-				</div>
-			</div>
+      <div className="bet-slip__content">
+        <Scrollbars
+          style={{ height: listHeight() + "px" }}
+          autoHide
+          autoHideTimeout={1000}
+          autoHideDuration={200}
+          renderThumbVertical={(props) => (
+            <div {...props} className="thumb-vertical" />
+          )}
+          renderView={(props) => <div {...props} className="view" />}
+        >
+          {list.map((item) => (
+            <BetSlipOdd data={item} key={item.id} listType={listType} />
+          ))}
+          {/* {!!listType || <BetSlipOddCounter amount={amount} setAmount={setAmount} onChangeInput={onChangeInput}/> }
+				{!!listType || <BetSlipOddTotal amount={amount} multiplayer={multiplayer}/>} */}
+        </Scrollbars>
+      </div>
 
-			<div className="bet-slip__content">
-				<Scrollbars
-					style={{ height: listHeight() + "px" }}
-					autoHide
-					autoHideTimeout={1000}
-					autoHideDuration={200}
-					renderThumbVertical={(props) => (
-						<div {...props} className="thumb-vertical" />
-					)}
-					renderView={(props) => <div {...props} className="view" />}
-				>
-					{list.map((item) => (
-						<BetSlipOdd data={item} key={item.id} listType={listType} />
-					))}
-					{/* {!!listType || (
-						<BetSlipOddCounter
-							amount={amount}
-							setAmount={setAmount}
-							onChangeInput={onChangeInput}
-						/>
-					)} */}
-					{!!listType && (
-						<BetSlipOddTotal amount={amount} multiplayer={multiplayer} />
-					)}
-				</Scrollbars>
-			</div>
-
-			<div className="bet-slip__footer">
-				<button className="button">Make a bet</button>
-			</div>
-		</div>
-	);
+      <div className="bet-slip__footer">
+        <button className="button">Make a bet</button>
+      </div>
+    </div>
+  );
 }
 
 export default BetSlip;
--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -6,115 +6,116 @@ import "./Game.scss";
 import { clickOnMultiplayer } from "../../redux/slices/betSlipListSlice";
 
 function Game({ dataGame, list }) {
-	const multiplayer = 1.85;
-	const { status } = dataGame;
+  const multiplayer = 1.85;
+  const { status } = dataGame;
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const activeLeftBtn = list.some((obj) => obj.id === dataGame.id + "_0");
-	const activeRightBtn = list.some((obj) => obj.id === dataGame.id + "_1");
+  const activeLeftBtn = list.some((obj) => obj.id === dataGame.id + "_0");
+  const activeRightBtn = list.some((obj) => obj.id === dataGame.id + "_1");
 
-	function clickHandler(btn) {
-		const btnId = btn.target.id;
+  function clickHandler(btn) {
+    const btnId = btn.target.id;
 
-		const betSlipData = {
-			id: dataGame.id + "_" + btnId,
-			firstTeam: dataGame.opponents[0]?.opponent.name || "TBD",
-			secondTeam: dataGame.opponents[1]?.opponent.name || "TBD",
-			teamWinner: dataGame.opponents[btnId]?.opponent.name || "TBD",
-			event: "Match winner",
-			multiplayer: 1.85,
-		};
-		dispatch(clickOnMultiplayer(betSlipData));
-	}
+    const betSlipData = {
+      id: dataGame.id + "_" + btnId,
+      firstTeam: dataGame.opponents[0]?.opponent.name || "TBD",
+      secondTeam: dataGame.opponents[1]?.opponent.name || "TBD",
+      teamWinner: dataGame.opponents[btnId]?.opponent.name || "TBD",
+      event: "Match winner",
+      multiplayer: 1.85,
+      amount: 0,
+    };
+    dispatch(clickOnMultiplayer(betSlipData));
+  }
 
-	return (
-		<div className="match">
-			<div className="match-header">
-				<div className="match-date">
-					<span className="date-time">
-						{status === "running"
-							? "LIVE"
-							: dataGame?.begin_at?.slice(-9, -4) || "Upcom"}
-					</span>
-					<span className="date-day">
-						{status === "running"
-							? ""
-							: dataGame?.begin_at?.slice(5, 10) || "Soon"}
-					</span>
-				</div>
-				<div className="match-game">
-					<img
-						width={22}
-						height={22}
-						src={`img/${dataGame.videogame.slug}.webp`}
-						alt={dataGame.videogame.slug}
-					/>
-				</div>
-			</div>
+  return (
+    <div className="match">
+      <div className="match-header">
+        <div className="match-date">
+          <span className="date-time">
+            {status === "running"
+              ? "LIVE"
+              : dataGame?.begin_at?.slice(-9, -4) || "Upcom"}
+          </span>
+          <span className="date-day">
+            {status === "running"
+              ? ""
+              : dataGame?.begin_at?.slice(5, 10) || "Soon"}
+          </span>
+        </div>
+        <div className="match-game">
+          <img
+            width={22}
+            height={22}
+            src={`img/${dataGame.videogame.slug}.webp`}
+            alt={dataGame.videogame.slug}
+          />
+        </div>
+      </div>
 
-			<div className="match-body">
-				<div className="select">
-					<span className="select-event">Event</span>
-					<span className="select-tournament">{dataGame.league.name}</span>
-				</div>
-				<div className="teamLeft team">
-					<div className="team-name">
-						{dataGame.opponents[0]?.opponent.name || "TBD"}
-					</div>
-					<div className="team-logo">
-						<img
-							width={24}
-							height={24}
-							src={
-								dataGame.opponents[0]?.opponent.image_url || "../img/user.svg"
-							}
-							alt="logo"
-						/>
-					</div>
-					<div
-						id="0"
-						className={"team-btn " + (activeLeftBtn ? "active" : "")}
-						onClick={clickHandler}
-						data-state="unactive"
-					>
-						{multiplayer}
-					</div>
-				</div>
-				<div className="draw">0:0</div>
-				<div className="teamRight team">
-					<div className="team-name">
-						{dataGame.opponents[1]?.opponent.name || "TBD"}
-					</div>
-					<div className="team-logo">
-						<img
-							width={24}
-							height={24}
-							src={
-								dataGame.opponents[1]?.opponent.image_url || "../img/user.svg"
-							}
-							alt="logo"
-						/>
-					</div>
-					<div
-						id="1"
-						className={"team-btn " + (activeRightBtn ? "active" : "")}
-						onClick={clickHandler}
-					>
-						{multiplayer}
-					</div>
-				</div>
-			</div>
-			<div className="match-more">
-				<button className="more-btn">
-					<span className="more-counter">23</span>
-					<div className="more-down">
-						<img width={15} height={15} src="../img/down.svg" alt="down" />
-					</div>
-				</button>
-			</div>
-		</div>
-	);
+      <div className="match-body">
+        <div className="select">
+          <span className="select-event">Event</span>
+          <span className="select-tournament">{dataGame.league.name}</span>
+        </div>
+        <div className="teamLeft team">
+          <div className="team-name">
+            {dataGame.opponents[0]?.opponent.name || "TBD"}
+          </div>
+          <div className="team-logo">
+            <img
+              width={24}
+              height={24}
+              src={
+                dataGame.opponents[0]?.opponent.image_url || "../img/user.svg"
+              }
+              alt="logo"
+            />
+          </div>
+          <div
+            id="0"
+            className={"team-btn " + (activeLeftBtn ? "active" : "")}
+            onClick={clickHandler}
+            data-state="unactive"
+          >
+            {multiplayer}
+          </div>
+        </div>
+        <div className="draw">0:0</div>
+        <div className="teamRight team">
+          <div className="team-name">
+            {dataGame.opponents[1]?.opponent.name || "TBD"}
+          </div>
+          <div className="team-logo">
+            <img
+              width={24}
+              height={24}
+              src={
+                dataGame.opponents[1]?.opponent.image_url || "../img/user.svg"
+              }
+              alt="logo"
+            />
+          </div>
+          <div
+            id="1"
+            className={"team-btn " + (activeRightBtn ? "active" : "")}
+            onClick={clickHandler}
+          >
+            {multiplayer}
+          </div>
+        </div>
+      </div>
+      <div className="match-more">
+        <button className="more-btn">
+          <span className="more-counter">23</span>
+          <div className="more-down">
+            <img width={15} height={15} src="../img/down.svg" alt="down" />
+          </div>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Game;
